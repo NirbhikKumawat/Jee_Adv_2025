dc83aa8d688374d25a47b72f75bc71d92cb82ba3
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-09-16 22:17:51 +0530

    Succesfully connected the database to Express

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: PORT,
+    port: process.env.DB_PORT||5432,
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
@@ -22,6 +22,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+pool.connect((err,client,release)=>{
+    if(err){
+        console.error('Error acquiring client',err.stack);
+    }
+    console.log('Connected to PostreSQL database');
+    release();
+})
+
+
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
 })
+
