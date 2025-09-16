6e29326d1f1704f8b0011cf165484fc6f661b86b
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-09-16 19:06:50 +0530

    Bare Bones,Server started running and listening . Nothing else.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Happy developing ✨')
+const express = require('express');
+const cors = require('cors');
+const {Pool} = require('pg');
+//const helmet = require('helmet');
+//const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: PORT,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
