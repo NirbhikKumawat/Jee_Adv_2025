2224e704f483447e10684f92600755a2c03ccdba
Nirbhik The Nice <nirbhikkumawat@gmail.com>
2025-09-17 10:48:45 +0530

    Added to view speific iit info

--- index.js
+++ index.js
@@ -30,8 +30,69 @@ pool.connect((err,client,release)=>{
     release();
 })
 
+app.get('/users',async(req,res)=>{
+    try{
+        const result = await pool.query('SELECT * FROM jeeadv_2025_seat_allotment');
+        res.json({
+            success:true,
+            data:result.rows,
+            count:result.rowCount
+        });
+    }catch(err){
+        console.error('Error fetching users',err);
+        res.status(500).json({
+            success:false,
+            error: 'Internal server error'
+        })
+    }
+})
+
+app.get('/iit/:iitId',async(req,res)=>{
+    try{
+        const {iitId} = req.params;
+        const result = await pool.query('SELECT * FROM jeeadv_2025_seat_allotment WHERE iit=$1 ORDER BY rank,ews_rank,obc_rank,sc_rank,st_rank',[iitId]);
+        res.json({
+            success:true,
+            data:result.rows,
+            count:result.rowCount
+        });
+    }catch(err){
+        console.error('Error fetching users',err);
+        res.status(500).json({
+            success:false,
+            error: 'Internal server error'
+        })
+    }
+})
+
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
 })
+app.get('/users/:rank',async(req,res)=>{
+    try{
+        const { rank } =req.params;
+        const result = await pool.query('SELECT * FROM jeeadv_2025_seat_allotment WHERE rank = $1',[rank]);
+        if(result.rowCount===0){
+            return res.status(404).json({
+                success:false,
+                error : "No record found"
+            });
+        }
+        res.json({
+            success:true,
+            data:result.rows[0],
+        })
+    }catch(err){
+        console.error('Error fetching users',err);
+        res.status(500).json({
+            success:false,
+            error: 'Internal server error'
+        })
+    }
+})
+
+
 
